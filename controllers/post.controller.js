82c35e237f52147b82e450c933571efc9d81afb1
unknown <IECISA@BO3539006DC0076.iecisa.corp>
2020-09-18 21:05:16 -0500

    Implementacion funcion crear post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,4 +1,44 @@
-                /**
-                 * TASK:
-                 * IMPLEMENT THE CONTROLLER 
-                 */
+/**
+ * TASK:
+ * IMPLEMENT THE CONTROLLER 
+ */
+const dbManager = require('../database.config/database.manager');
+
+/** 
+ * Creation of a post
+ * @param {*} postObject JSON Object with Post info
+ */
+
+ async function createPost(req, res){
+
+     //CHECK IF THE REQUEST BODY IS EMPTY
+     if(!req.body){
+         res.status(400).send({
+             message: "Request body is empty"
+         });
+         return;
+     }
+
+     //CREATING THE OBJECT TO PERSIST 
+     const newPostObject={
+         message: req.body.message,
+         published_date: req.body.published_date
+     }
+
+     //
+     dbManager.Post.create(newPostObject).then(
+         data => {
+             res.send(data);
+         }
+     ).catch(
+         e => {
+             console.log(e);
+             res.status(500).send({
+                 messsage: "Some error ocurred"
+             });
+         }
+     );
+}
+
+exports.createPost = createPost;
+
--- routes/posts.route.js
+++ routes/posts.route.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const postController = require('../controllers/post.controller')
 
 /**
- * TASK:
- * CREATE THE ROUTES _________________________________________________________ 
+ * POST Route to create post
  */
-  
+router.post ('/', postController.createPost);
+
 
 module.exports = router;
   
