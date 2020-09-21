f9014165d073e4f80b6fd7b91c1152f4e9f9583f
Jhballen <Jhballen@poligran.edu.co>
2020-09-21 16:32:09 -0500

    Despliegue heroku

--- database.config/db.config.js
+++ database.config/db.config.js
@@ -1,11 +1,13 @@
 /**
  * Database Configuration Object
  */
-const dbconfig = {
-        HOST: "localhost",
-        USER: "jhballen",
-        PASSWORD: "jhon123",
-        DB: "blackpoli",
+
+//Heroku mysql://bb9f2e92f04f43:4fbb7189@us-cdbr-east-02.cleardb.com/heroku_298428e67c2ec80?reconnect=true
+ const dbconfig = {
+        HOST: "us-cdbr-east-02.cleardb.com",
+        USER: "bb9f2e92f04f43",
+        PASSWORD: "4fbb7189",
+        DB: "heroku_298428e67c2ec80",
         dialect: "mysql",
         pool: {
           max: 5,
